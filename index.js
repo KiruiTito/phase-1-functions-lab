d0466f095f62ce75dc1b16c2123a77c49f0fb1d2
KiruiTito <titokirui402@gmail.com>
2023-03-30 14:46:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution in this file!
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+    let fare = 0;
+    if (distance <= 400) {
+      fare = 0;
+    } else if (distance > 400 && distance <= 2000) {
+      fare = (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+      fare = 25;
+    } else if (distance > 2500) {
+      return 'cannot travel that far';
+    }
+    return fare;
+  }
+  
